03f21bc91669cb5f98c69b5f8519aafafca46a6b
xmpulcar610-prog <xmpulcar610@ieshnosmachado.org>
2025-10-24 14:48:11 +0200

    Cambios 24/10

--- Boletin1/EJ 3/ej_3.js
+++ Boletin1/EJ 3/ej_3.js
@@ -2,6 +2,5 @@ function mostrarDatos() {
       let nombre = document.getElementById("nombre").value;
       let edad = document.getElementById("edad").value;
       
-      document.getElementById("resultado").innerHTML =
-        "Tu nombre es: " + nombre + "<br>Tu edad es: " + edad + " años";
+      document.getElementById("resultado").innerHTML = "Tu nombre es: " + nombre + "<br>Tu edad es: " + edad + " años";
     }
