9047b862e7262b7ea1891654dd55f71a44d1da9b
xmpulcar610-prog <xmpulcar610@ieshnosmachado.org>
2025-10-26 20:54:46 +0100

    Cambios 26/10

--- Boletin2/EJ 1/ej_1.js
+++ Boletin2/EJ 1/ej_1.js
@@ -1,7 +1,19 @@
 let nombre= prompt("Introduzca su nombre y apellidos");
 
-let tamaño = nombre.length;
-let minusculas = 
+let tamaño = nombre.replaceAll(" ", "").length;
+let minusculas = nombre.toLowerCase();
+let mayusculas = nombre.toUpperCase();
+let nombre_por_partes = nombre.trim().split(" ");
 
-document.getElementById("salida").innerHTML = tamaño-2;
+let nombre_propio = nombre_por_partes[0];
+let apellido1 = nombre_por_partes[1];
+let apellido2 = nombre_por_partes[2];
+
+let usuario = nombre_propio[0].toLowerCase() + apellido1.slice(0,3).toLowerCase() + apellido2.slice(0,3).toLowerCase();
+
+document.getElementById("salida1").innerHTML = "El tamaño de la cadena es: " +tamaño;
+document.getElementById("salida2").innerHTML = "Esta es la cadena en minúsculas: " + minusculas + "<br>" + 
+                                                "Esta es la cadena en mayúsculas: " +mayusculas;
+document.getElementById("salida3").innerHTML = nombre_por_partes.join("<br>");
+document.getElementById("salida4").innerHTML = usuario;
 
