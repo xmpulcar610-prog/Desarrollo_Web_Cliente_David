0860a2b15d997ac757f263cac0970cef1b4875d0
xmpulcar610-prog <xmpulcar610@ieshnosmachado.org>
2025-11-11 13:55:31 +0100

    Cambios 11/11

--- TEMA 2/BOLETÍN 1/EJ 04/ej_04.js
+++ TEMA 2/BOLETÍN 1/EJ 04/ej_04.js
@@ -1,1 +1,10 @@
-agregarProvincia()
+function agregarProvincia()
+    {
+        let codigo = frmEntrada.txtCodigo.value.trim()
+        let provincia = frmEntrada.txtProvincia.value.trim()
+
+        if (!existeProvincia(codigo))
+            {
+                
+            }
+    }
