c246ae5d77617ad21787f3ef2888f8f91b8a1954
xmpulcar610-prog <xmpulcar610@ieshnosmachado.org>
2025-11-18 14:10:58 +0100

    Cambios 18/11

--- TEMA 2/BOLETÍN 1/EJ 08/ej_08.js
+++ TEMA 2/BOLETÍN 1/EJ 08/ej_08.js
@@ -7,6 +7,7 @@ function impedirDigitos(event)
 
         if (event.key >= "0" && event.key <= "9") 
             {
+                console.log(event.key)
                 event.preventDefault(); 
             }
     }
